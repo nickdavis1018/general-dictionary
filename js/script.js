bf8b851472547c9f2a15ec0fbf92fdd98cece477
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-04 01:33:18 -0500

    updated styling, tweaked Jquery

--- js/script.js
+++ js/script.js
@@ -30,39 +30,45 @@ function getWord(event){
       render();
     },
     function(error){
-    $word.text("Error!");
-    $phonetic.text("Error!");
-    $origin.text("Error!");
-    $synonyms.text("Error!");
-    $definition.text("Error!");
+    $definition.text(`Something went wrong. "${$input.val()}" did not match our database and failed to return any results. Please try again`);
     }
   )
 }
 
 function render() {
-  $word.text(`Word: ${wordData[0].word}`);
-  $phonetic.text(`Phonetic: ${wordData[0].phonetic}`);
-  $origin.text(`Origin: ${wordData[0].origin}`)
+  $word.html(`${wordData[0].word}`);
+  if(wordData[0].phonetic === undefined){
+  $phonetic.html(`Phonetic Unkown`)
+  }
+  else{
+  $phonetic.html(`/${wordData[0].phonetic}/`)}
+  if(wordData[0].origin === undefined){
+  $origin.html(`Origin Unkown`)
+  }
+  else{
+  $origin.html(`Origin <br> ${wordData[0].origin}`)}
   $sound.empty()
   $sound.html(`<audio controls id="pronounce"><source src="https:${wordData[0].phonetics[0].audio}" type="audio/mpeg"</audio>"`)
     for(let i=0; i < wordData[0].meanings.length; i++){
-    const speech = wordData[0].meanings[i].partOfSpeech
       for(let x=0; x < wordData[0].meanings[i].definitions.length; x++){
-      $definition.append(`<p class="word-css" id="define${x}">${speech}; <br> ${wordData[0].meanings[i].definitions[x].definition}</p>`)
-      $definition.css("font-weight", "bold")
-      if(wordData[0].meanings[i].definitions[x].example !== false){
-      const $exDataString = wordData[0].meanings[i].definitions[x].example
-      $definition.append(`<p class="ex-css" id="example-text${x}">ex: "${$exDataString}"</p>`)}
-      if(wordData[0].meanings[i].definitions[x].synonyms !== false){
-      let $synData = wordData[0].meanings[i].definitions[x].synonyms.slice(0, 20)
-      let $synDataString = $synData.join(', ')
-      $definition.append(`<p class="syn-css" id="syndesc${x}">${$synDataString}</p>`)}
-      if(wordData[0].meanings[i].definitions[x].antonyms !== false){
-      let $antData = wordData[0].meanings[i].definitions[x].antonyms.slice(0, 10)
-      let $antDataString = $antData.join(', ')
-      $definition.append(`<p class="ant-css" id="antdesc${x}">${$antDataString}</p></div>`)
-      }}
+        $definition.append(`<div class="word-css-class" id="define${i}-${x}"><strong>${wordData[0].word}</strong> <i>(${wordData[0].meanings[i].partOfSpeech})</i> <br><i>${wordData[0].meanings[i].definitions[x].definition}</i></div>`)
+        if(wordData[0].meanings[i].definitions[x].example !== undefined){
+          const $exDataString = wordData[0].meanings[i].definitions[x].example
+          $(`#define${i}-${x}`).append(`<p class="ex-css"><i>Example: "${$exDataString}"</i></p>`)}
+        if(wordData[0].meanings[i].definitions[x].synonyms.length > 0){
+          let $synData = wordData[0].meanings[i].definitions[x].synonyms.slice(0, 20)
+          let $synDataString = $synData.join(', ')
+          $(`#define${i}-${x}`).append(`<p class="syn-css"><i>Synonyms: ${$synDataString}</i></p>`)}
+        if(wordData[0].meanings[i].definitions[x].antonyms.length > 0){
+          let $antData = wordData[0].meanings[i].definitions[x].antonyms.slice(0, 10)
+          let $antDataString = $antData.join(', ')
+          $(`#define${i}-${x}`).append(`<p class="ant-css"><i>Antonyms: ${$antDataString}</i></p></div>`)}
+        $(`#define${i}-${x}`).css("border", "solid")
+        $(`#define${i}-${x}`).css("border-color", "black")
+        $definition.css("border", "solid")
+        $definition.css("border-color", "black")
     }
+  }
 }
 function randomWord(){
   $.ajax({
@@ -75,21 +81,20 @@ function randomWord(){
       }).then(
           function(data){
           randomDataRef = data;
-          $randWord.html(`<strong>${randomDataRef[0].word}</strong>`)
+          $randWord.html(`<i><strong>${randomDataRef[0].word}</strong></i>`)
           for(let i=0; i < randomDataRef[0].meanings.length; i++){
             const randSpeech = randomDataRef[0].meanings[i].partOfSpeech
               for(let x=0; x < randomDataRef[0].meanings[i].definitions.length; x++){
-              $randWordDesc.append(`<p class="rand-word-css" id="rand-define${x}"><strong></strong>${randSpeech}; <br>${randomDataRef[0].meanings[i].definitions[x].definition}</p>`)
+              $randWordDesc.append(`<p class="rand-word-css" id="rand-define${x}"><strong></strong>${randSpeech};<br>${randomDataRef[0].meanings[i].definitions[x].definition}</p>`)
               if(randomDataRef[0].meanings[i].definitions[x].example){
               const $exRandDataString = randomDataRef[0].meanings[i].definitions[x].example
               $randWordDesc.append(`<p class="rand-ex-css" id="rand-example-text${x}"><i>ex: "${$exRandDataString}"</p></i>`)}
-              
           }}},
           function(error){
           randomWord();
             })},
     function(error){
-      $randWord.html("Error. Word retrieval failed. Please try again.")})}
+      $randWord.html("Error. Word retrieval failed. Please refresh the page and try again.")})}
 
 function clearRandom(){
   $randWord.empty()
